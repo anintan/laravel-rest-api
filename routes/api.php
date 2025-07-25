14261a4fbbd1286e3f522eb2605ec351670ffb2b
anintan <tansyntdw@gmail.com>
2025-07-25 14:00:02 +0700

    tambah route api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+Route::get('/hello', function () {
+    return response()->json(['message' => 'Halo dari Laravel API!']);
+});
