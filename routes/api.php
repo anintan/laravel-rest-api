8f4569c3eaab5a0d1c5b99f16c5c69d158c5b09a
anintan <tansyntdw@gmail.com>
2025-07-25 16:56:16 +0700

    tambah route users dan  file usercontroller.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
 Route::get('/hai', function () {
     return response()->json(['message' => 'Halo dari Laravel API!']);
 });
+Route::get('/users', [UserController::class, 'index']);
+
